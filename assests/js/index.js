da690f1b24d57edee82a72187cb42b3a6fcd00a6
akhilbharambe <akhilbharambe444@gmail.com>
2023-06-30 19:23:02 +0530

    portfolio completed

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,4 +1,30 @@
 const btn = document.getElementById("ab-btn");
 btn.addEventListener("click", function(){
-    window.location.href="#about";
+    window.location.href="#more-about";
 })
+// JS FOR SCROLL-UP
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+// Nav hamburgerburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
